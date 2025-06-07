7dfe360979e4dc4295970f408f4344ce63a56a52
tre3iii <lilshed3@gmail.com>
2025-06-07 15:17:54 -0700

    Update Stack.js

--- abstracts/Stack.js
+++ abstracts/Stack.js
@@ -1,3 +1,11 @@
 //A list that operates under the LIFO principle
 
-//Think of an array that pushes and pops from the top
+//Think of an array that pushes and pops from the top. Push and Pop
+
+/*
+These come from a computer concept known as a stack, 
+which works like a spring-loaded tray dispenser in a cafeteria. 
+When someone returns a tray to the stack, its weight pushes the platform down. 
+When someone needs a tray, he takes it from the top of the stack, 
+and the remaining trays pop up a little bit.
+*/
